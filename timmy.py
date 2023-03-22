f30b602e56e835581afa0a4f64935e8502f94415
Timmy <tl674@cornell.edu>
2023-03-22 00:07:14 -0400

    Combined 3 days worth of data

--- timmy.py
+++ timmy.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 
 # df = pd.read_csv('61bcb8fd-1630-46c2-857a-db3d51ac73e0.csv', sep = ';')
 
-df_day2 = pd.read_csv('./island-data-bottle-round-1/prices_round_1_day_-2.csv', sep = ';')
+df_day0 = pd.read_csv('./island-data-bottle-round-1/prices_round_1_day_-2.csv', sep = ';')
 df_day1 = pd.read_csv('./island-data-bottle-round-1/prices_round_1_day_-1.csv', sep = ';')
-df_day0 = pd.read_csv('./island-data-bottle-round-1/prices_round_1_day_0.csv', sep = ';')
+df_day2 = pd.read_csv('./island-data-bottle-round-1/prices_round_1_day_0.csv', sep = ';')
+
+df_day1['timestamp'] = df_day1['timestamp'] + 999900
+df_day2['timestamp'] = df_day2['timestamp'] + 2 * 999900
 
 # df = df_day2.merge(df_day1, on=["day", "timestamp", "product", "bid_price_1","bid_volume_1","bid_price_2","bid_volume_2","bid_price_3","bid_volume_3","ask_price_1","ask_volume_1","ask_price_2","ask_volume_2","ask_price_3","ask_volume_3","mid_price","profit_and_loss"], how='outer')
-df = pd.concat([df_day2, df_day1, df_day0], axis=0)
+df = pd.concat([df_day0, df_day1, df_day2], axis=0)
 # df.to_csv("final.csv",index=False)
 
 x = df[df['product'] == 'BANANAS']['timestamp']
@@ -20,7 +23,7 @@ BOLU = y + 2 * std
 BOLD = y - 2 * std
 
 plt.plot(x, df[df['product'] == 'BANANAS']['mid_price'], color = 'r')
-plt.plot(x, y)
+#plt.plot(x, y)
 plt.plot(x, BOLU)
 plt.plot(x, BOLD)
 plt.show()
