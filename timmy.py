6a52d6b9d460fd16feafae3c60dc957cc45e4bd6
Aayush <68517064+Aayush-Agnihotri@users.noreply.github.com>
2023-03-21 22:58:16 -0500

    Add files via upload

--- timmy.py
+++ timmy.py
@@ -2,12 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('61bcb8fd-1630-46c2-857a-db3d51ac73e0.csv', sep = ';')
+
+# df = pd.read_csv('61bcb8fd-1630-46c2-857a-db3d51ac73e0.csv', sep = ';')
+
+df_day2 = pd.read_csv('./island-data-bottle-round-1/prices_round_1_day_-2.csv', sep = ';')
+df_day1 = pd.read_csv('./island-data-bottle-round-1/prices_round_1_day_-1.csv', sep = ';')
+df_day0 = pd.read_csv('./island-data-bottle-round-1/prices_round_1_day_0.csv', sep = ';')
+
+# df = df_day2.merge(df_day1, on=["day", "timestamp", "product", "bid_price_1","bid_volume_1","bid_price_2","bid_volume_2","bid_price_3","bid_volume_3","ask_price_1","ask_volume_1","ask_price_2","ask_volume_2","ask_price_3","ask_volume_3","mid_price","profit_and_loss"], how='outer')
+df = pd.concat([df_day2, df_day1, df_day0], axis=0)
+# df.to_csv("final.csv",index=False)
+
 x = df[df['product'] == 'BANANAS']['timestamp']
 y = df[df['product'] == 'BANANAS']['mid_price'].rolling(window = 100).mean()
 std = df[df['product'] == 'BANANAS']['mid_price'].rolling(window = 100).std()
-BOLU = y + 1.5 * std
-BOLD = y - 1.5 * std
+BOLU = y + 2 * std
+BOLD = y - 2 * std
 
 plt.plot(x, df[df['product'] == 'BANANAS']['mid_price'], color = 'r')
 plt.plot(x, y)
