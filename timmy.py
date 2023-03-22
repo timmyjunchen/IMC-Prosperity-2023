7ff5306290bef14c6f34f3b9e1b9cfd2656d2344
Timmy <tl674@cornell.edu>
2023-03-21 23:37:54 -0400

    Changed Bollinger Band Range
    -Changed standard deviation from 2 to 1.5

--- timmy.py
+++ timmy.py
@@ -6,8 +6,8 @@ df = pd.read_csv('61bcb8fd-1630-46c2-857a-db3d51ac73e0.csv', sep = ';')
 x = df[df['product'] == 'BANANAS']['timestamp']
 y = df[df['product'] == 'BANANAS']['mid_price'].rolling(window = 100).mean()
 std = df[df['product'] == 'BANANAS']['mid_price'].rolling(window = 100).std()
-BOLU = y + 2 * std
-BOLD = y - 2 * std
+BOLU = y + 1.5 * std
+BOLD = y - 1.5 * std
 
 plt.plot(x, df[df['product'] == 'BANANAS']['mid_price'], color = 'r')
 plt.plot(x, y)
